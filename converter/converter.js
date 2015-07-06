024aa13df8c71d07852c117da7469235c86b8e0e
vovabob <bobruskin@gmail.com>
2015-07-06 16:23:08 +1200

    Initial commit
    Changes to be committed:
      new file:   converter/background.js
      new file:   converter/converter.js
      new file:   converter/index.html
      new file:   converter/manifest.json

---
+++ converter/converter.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  document.querySelector("#convert").addEventListener("click",
+    function() {
+      var meters = document.querySelector("#meters");
+      var feet = document.querySelector("#feet");
+      feet.value = meters.value * 3.28084;
+    }
+  );  
+};
